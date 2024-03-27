2c54a1a0c1dc66d2d4003ea2ffbf2f247069a11b
Kim Sanghae <pelikan@pusan.ac.kr>
2024-03-27 21:28:46 +0900

    Fix some codes

--- week03/Assignments/week3_4.py
+++ week03/Assignments/week3_4.py
@@ -1,12 +1,11 @@
 prime_number = int(input('숫자를 입력하세요 : '))
 if prime_number == 1:
     print(f'{prime_number}는 소수가 아닙니다')
-elif prime_number == 2:
-    print(f'{prime_number}는 소수입니다')
 else:
-    for i in range(2, prime_number):
+    for i in range(2, prime_number + 1):
+        if i == prime_number:
+            print(f'{prime_number}는 소수입니다.')
+            break
         if prime_number % i == 0:
             print(f'{prime_number}는 소수가 아닙니다')
             break
-        if i == prime_number - 1:
-            print(f'{prime_number}는 소수입니다')
