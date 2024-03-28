1e659bfe922cbb7f6ddf21be457f61515358c53a
Kim Sanghae <pelikan@pusan.ac.kr>
2024-03-28 17:26:58 +0900

    Change prime number flag bit

--- week03/Assignments/week3_4.py
+++ week03/Assignments/week3_4.py
@@ -1,11 +1,16 @@
-prime_number = int(input('숫자를 입력하세요 : '))
-if prime_number == 1:
-    print(f'{prime_number}는 소수가 아닙니다')
+n = int(input('숫자를 입력하세요 : '))
+is_prime = True
+if n == 1:
+    is_prime = False
+elif  n != 2 and n % 2 == 0:
+    is_prime = False
 else:
-    for i in range(2, prime_number + 1):
-        if i == prime_number:
-            print(f'{prime_number}는 소수입니다.')
+    for i in range(3, n, 2):
+        if n % i == 0:
+            is_prime = False
             break
-        if prime_number % i == 0:
-            print(f'{prime_number}는 소수가 아닙니다')
-            break
+
+if is_prime:
+    print(f'{n}은/는 소수입니다.')
+else:
+    print(f'{n}은/는 소수가 아닙니다.')
