069b142f26ec5e47ed9222e07543db6fd9fcb241
Kim Sanghae <pelikan@pusan.ac.kr>
2024-04-02 17:29:13 +0900

    week04 assignment review

--- week04/sourcecode/week04_insertion_sort.py
+++ week04/sourcecode/week04_insertion_sort.py
@@ -2,8 +2,8 @@
 input_list = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 ### driven code; i번째 칸을 정렬하라. 큰 의미는 X
 for i in range(1, len(input_list)):
-    for j in range(i,0,-1):
-        if input_list[j] > input_list[j -1]:
+    for j in range(i,0,-1): # 인덱스를 거꾸로 가야 한다.
+        if input_list[j] < input_list[j -1]:
             input_list[j], input_list[j - 1] = input_list[j - 1],input_list[j]
 
 print(input_list)
--- week04/sourcecode/week04_list.py
+++ week04/sourcecode/week04_list.py
@@ -10,15 +10,32 @@
 
 list1 = list()
 list2 = []
-list3 = list((1,2,3))
-list4 = list(range(1,10))
+list3 = list((1, 2, 3))
+list4 = list(range(1, 10))
 list5 = list('ABCDEF')
 
 # 대괄호가 리스트임을 나타낸다.
-n_list = [11,12,13,14,15]
+n_list = [11, 12, 13, 14, 15]
 # [index] => 양수 or 음수
-print(n_list[-1]) # result : [11, 12, 13, 14, 15]
+print(n_list[-1])  # result : [11, 12, 13, 14, 15]
 
+a_list = [10, 20, 30, 40, 50, 60, 70, 80]
+a_list[1:5]  # [20, 30, 40, 50] # 슬라이싱은 원본을 복사한다.
+a_list[0:1]  # [10]
+a_list[0:2]  # [10, 20]
+a_list[0:5]  # [10, 20, 30, 40, 50]
+a_list[1:]  # [20, 30, 40, 50, 60, 70, 80]
+a_list[:5]  # [10, 20, 30, 40, 50]
 
+n_list = [10,20,30,40]
+n_list.append(50)
+last_element = n_list.pop()
+n_list.remove(20)
+print(n_list)
+print(last_element)
 
+print(10 in n_list)
+print(n_list.index(30))
 
+for i in n_list:
+    print(f"{i:8d}",end="")
